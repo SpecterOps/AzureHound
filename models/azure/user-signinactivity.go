4b0bd4b2b83e06ab73bc017ebd17b66387ed0544
Alyx Holms <aholms@specterops.io>
2026-02-13 09:28:43 -0800

    fix: pointer to string was required due to the model from MS having null values

--- models/azure/user-signinactivity.go
+++ models/azure/user-signinactivity.go
@@ -2,10 +2,10 @@ package azure
 
 // SignInActivity represents Microsoft Graph's `signInActivity` object returned on the user entity.
 type SignInActivity struct {
-	LastSignInDateTime                string `json:"lastSignInDateTime,omitempty"`
-	LastSignInRequestId               string `json:"lastSignInRequestId,omitempty"`
-	LastNonInteractiveSignInDateTime  string `json:"lastNonInteractiveSignInDateTime,omitempty"`
-	LastNonInteractiveSignInRequestId string `json:"lastNonInteractiveSignInRequestId,omitempty"`
-	LastSuccessfulSignInDateTime      string `json:"lastSuccessfulSignInDateTime,omitempty"`
-	LastSuccessfulSignInRequestId     string `json:"lastSuccessfulSignInRequestId,omitempty"`
+	LastSignInDateTime                *string `json:"lastSignInDateTime,omitempty"`
+	LastSignInRequestId               *string `json:"lastSignInRequestId,omitempty"`
+	LastNonInteractiveSignInDateTime  *string `json:"lastNonInteractiveSignInDateTime,omitempty"`
+	LastNonInteractiveSignInRequestId *string `json:"lastNonInteractiveSignInRequestId,omitempty"`
+	LastSuccessfulSignInDateTime      *string `json:"lastSuccessfulSignInDateTime,omitempty"`
+	LastSuccessfulSignInRequestId     *string `json:"lastSuccessfulSignInRequestId,omitempty"`
 }
